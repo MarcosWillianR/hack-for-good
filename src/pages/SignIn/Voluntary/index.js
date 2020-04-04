ade28ff728bb7c1cb9dce11d138253ed4db28182
Marcos Willian <markusuuuu@gmail.com>
2020-04-04 06:09:10 -0300

    finish login component of companies and voluntaries (without style)

--- src/pages/SignIn/Voluntary/index.js
+++ src/pages/SignIn/Voluntary/index.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
+import { Form } from '@unform/web';
+import Input from '~/components/Form/Input';
+
+import validator from './validator';
 import { Container } from './styles';
 
 export default function Voluntary() {
+  const formRef = useRef(null);
+
+  async function handleSubmit(data, { reset }) {
+    await validator(data, formRef);
+
+    reset();
+  }
+
   return (
     <Container>
-      <h1>Logar como Voluntário</h1>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <Input name="login" placeholder="Login" />
+        <Input type="password" name="password" placeholder="Senha" />
+
+        <button type="submit">Enviar</button>
+      </Form>
     </Container>
   );
 }
