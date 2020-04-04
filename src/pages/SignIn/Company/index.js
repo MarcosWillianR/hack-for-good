235b493a02c12fb83cf92e97eea0f1da7890004f
Marcos Willian <markusuuuu@gmail.com>
2020-04-04 06:01:06 -0300

    initial pages for sign in

---
+++ src/pages/SignIn/Company/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Company() {
+  return (
+    <Container>
+      <h1>Logar como Empresa</h1>
+    </Container>
+  );
+}
---
+++ src/pages/SignIn/Voluntary/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Voluntary() {
+  return (
+    <Container>
+      <h1>Logar como Voluntário</h1>
+    </Container>
+  );
+}
