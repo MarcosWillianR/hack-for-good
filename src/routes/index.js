81c4a0be1b7600629648bb385dd6ffefca0e1ccf
Marcos Willian <markusuuuu@gmail.com>
2020-04-05 20:54:01 -0300

    improving route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,9 @@ import Route from './Route';
 
 import Home from '~/pages/Home';
 import SignIn from '~/pages/SignIn';
-import SignUp from '~/pages/SignUp';
+
+import SelectUserType from '~/pages/SignUp';
+import CompanySignUp from '~/pages/SignUp/Company';
 
 import Dashboard from '~/pages/Dashboard';
 
@@ -15,7 +17,9 @@ export default function Routes() {
         <Route path="/" exact component={Home} />
 
         <Route path="/login" component={SignIn} />
-        <Route path="/signup" component={SignUp} />
+
+        <Route path="/signup" component={SelectUserType} />
+        <Route path="/company-signup" component={CompanySignUp} />
 
         <Route path="/dashboard" component={Dashboard} isPrivate />
 
